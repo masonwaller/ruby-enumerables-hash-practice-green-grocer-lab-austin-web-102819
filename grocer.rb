c1f506fbd0d251c7b889cc6bd294a8a1cea55407
Mason Waller <masonwaller805@gmail.com>
2019-10-17 19:45:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ end
 def checkout(cart, coupons)
   hash_cart= consolidate_cart(cart)
   applied_coupons= apply_coupons(hash_cart, coupons)
-  apply_clear= appliy_clearance(applied_coupons)
+  apply_clear= apply_clearance(applied_coupons)
   total = apply_clear.reduce(0) { |sum, (key, value)| sum += value[:price]*value[:count]}
   total> 100? total * 0.9 : total
 end
