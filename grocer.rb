c86d8bc2051f8de2fa305ec2a969a6ea93239a19
Mason Waller <masonwaller805@gmail.com>
2019-10-17 19:26:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |item| 
-    if item[:clearance]
+    if item[:clearance] == true
       cart[:price]= (cart[:price] * 0.2).round(2)
     end 
   end
